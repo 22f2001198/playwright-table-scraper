20f7e21b9202421a40c47d21cd97d09e79ba72c6
22f2001198 <22f2001198@ds.study.iitm.ac.in>
2026-02-28 04:03:04 +0530

    Remove screenshot option and projects from config
    Removed screenshot option and projects configuration.

--- playwright.config.js
+++ playwright.config.js
@@ -2,9 +2,5 @@ module.exports = {
   use: {
     headless: true,
     viewport: { width: 1280, height: 720 },
-    screenshot: 'off',
   },
-  projects: [
-    { name: 'chromium', use: { ...devices['Desktop Chrome'] } }
-  ],
 };
